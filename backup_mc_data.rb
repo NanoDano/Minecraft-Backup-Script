4ab5f80876309b132bf6386c1debcf2d4e5cd4e4
NanoDano <nanodano@devdungeon.com>
2019-03-03 05:07:25 -0600

    Update backup_mc_data.rb
    Added cleaner logging, piped some warnings to /dev/null. Tested.

--- backup_mc_data.rb
+++ backup_mc_data.rb
@@ -11,7 +11,6 @@ DB_PASS = 'password'
 BACKUP_COUNT_LIMIT = 3
 REMOTE_SSH_HOST = '8.8.8.8'
 REMOTE_SSH_USER = 'user'
-REMOTE_SSH_KEYFILE = '/home/user/.ssh/id_rsa'
 
 
 $formatted_date = Time::now.strftime("%Y%m%d-%H%M%S")
@@ -20,7 +19,7 @@ $server_backup_filename = 'mc-server-' + $formatted_date + '.tar'
 
 
 def error_exit(msg)
-  full_message = "Error during backup: " + msg
+  full_message = "[-] Error during backup: " + msg
   puts full_message
   post_discord_webhook full_message
   exit(1)
@@ -28,7 +27,7 @@ end
 
 
 def post_discord_webhook(msg)
-  puts 'Posting message to discord: ' + msg
+  puts '[*] Posting message to discord: ' + msg
   uri = URI(DISCORD_WEBHOOK_URL)
   Net::HTTP.post_form(uri, 'content' => msg)
 end
@@ -36,25 +35,24 @@ end
 
 def archive_directory(dir_to_backup)
   puts '[*] Attempting to backup: ' + dir_to_backup + ' to ' + $server_backup_filename
-  `tar cf #{$server_backup_filename} #{dir_to_backup}`
-  puts $?.exitstatus
+  `tar cf #{$server_backup_filename} #{dir_to_backup} 2>/dev/null`
   if $?.exitstatus != 0
-    error_exit "Tar failed to create file."
+    error_exit "[-] Tar failed to create file."
   end
-  `rm #{$server_backup_filename}.gz` # prevent 'do you want to overwrite' message
+  `rm #{$server_backup_filename}.gz 2>/dev/null` # prevent 'do you want to overwrite' message
   `gzip #{$server_backup_filename}`
   if $?.exitstatus != 0
-    error_exit "Gzip failed on server backup."
+    error_exit "[-] Gzip failed on server backup."
   end
 end
 
 
 def archive_database(user, pass, name, host)
-  `mysqldump -u#{user} -p#{pass} -h#{host} #{name} > #{$mysql_output_filename}`
+  `mysqldump -u#{user} -p#{pass} -h#{host} #{name} > #{$mysql_output_filename} 2>/dev/null`
   if $?.exitstatus != 0
     error_exit 'Mysqldump failed.'
   end
-  `rm #{$mysql_output_filename}.gz`
+  `rm #{$mysql_output_filename}.gz 2>/dev/null`
   `gzip #{$mysql_output_filename}`
   if $?.exitstatus != 0
     error_exit 'Gzip failed on db.'
@@ -74,25 +72,28 @@ end
 
 
 def sync_with_remote
-  puts "Beginning sync with remote server #{REMOTE_SSH_HOST}"
-  `rsync . #{REMOTE_SSH_USER}@#{REMOTE_SSH_HOST}:backups/`
+  puts "[*] Syncing backups with remote server #{REMOTE_SSH_HOST}"
+  `rsync ./ #{REMOTE_SSH_USER}@#{REMOTE_SSH_HOST}:backups/ --delete --recursive`
   if $?.exitstatus != 0
-    error_exit 'Failed to sync backups remotely.'
+    error_exit '[-] Failed to sync backups remotely.'
   end
+  puts "[*] Done syncing backups with #{REMOTE_SSH_HOST}."
 end
 
 
 def main
+  puts "[*] Ruby backup script starting..."
   begin
     Dir::chdir(BACKUP_DESTINATION_DIR)
   rescue
-    error_exit "[-] Directory does not exist"
+    error_exit "[-] Directory does not exist" + BACKUP_DESTINATION_DIR
   end
   archive_directory DIR_TO_BACKUP
   archive_database DB_USER, DB_PASS, DB_NAME, DB_HOST
   rotate_backups '*.tar.gz'
   rotate_backups '*.mysql.gz'
   sync_with_remote
+  puts "[+] Ruby backup script completed successfully!"
 end
 
 
